74aa82c6eb6eaa0d1a61082d8f1dca091431dc6e
Rupesh <dasrupesh2124@gmail.com>
2025-12-28 11:05:42 +0530

    Made user schema and prepared for Mongo database connection

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,15 @@
+export const signup = (req, res) => {
+    // Handle signup logic here
+    res.send("Signup route");
+};
+
+export const login = (req, res) => {
+    // Handle login logic here
+    res.send("Login route");
+};
+
+export const logout = (req, res) => {
+    // Handle logout logic here
+    res.send("Logout route");
+};
+
