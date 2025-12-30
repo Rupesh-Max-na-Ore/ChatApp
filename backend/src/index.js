e18e283aa3ee84f4a676e10e925a0d1950db39d6
Rupesh <dasrupesh2124@gmail.com>
2025-12-30 16:15:03 +0530

    Made stores, components and pages

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
@@ -12,6 +13,9 @@ const PORT = process.env.PORT || 5001;
 app.use(express.json());
 // allow to parse cookies from incoming requests
 app.use(cookieParser());
+
+// enable CORS for all origins (you can restrict it in production)
+app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 
 // authentication routes
 app.use("/api/auth", authRoutes);
