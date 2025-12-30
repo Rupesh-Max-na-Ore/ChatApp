aa5635d56bd6f51eeaa9263e85be8f05cfa53b3f
Rupesh <dasrupesh2124@gmail.com>
2025-12-31 00:30:43 +0530

    Prepared app for production and/or deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,9 +7,12 @@ import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
 
+import path from "path";
+
 dotenv.config();
 
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 // Middleware to parse JSON requests, like signup requests
 // app.use(express.json());
 // app.use(express.json({ limit: "10mb" }));
@@ -27,6 +30,13 @@ app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 app.use("/api/auth", authRoutes);
 // message route
 app.use("/api/messages", messageRoutes);
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
+    });
+}
 
 server.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`);
